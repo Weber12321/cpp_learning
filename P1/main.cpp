2e550000c6b1236e41fab924d9947af3ae95b833
weber <doudi853@gmail.com>
2022-09-29 07:29:44 +0800

    2022-09-29

--- P1/main.cpp
+++ P1/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+extern int x;
+
 int main(){
     int favorite_number;
     std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
-    std::cout << "Amazing!! That's my favorite number, too." << std::endl;
+    // std::cout << "Amazing!! That's my favorite number, too."<< std::endl;
+    std::cout << "No, really!!, " << favorite_number << " is my favorite number, too."<< std::endl;
     return 0;
 }
